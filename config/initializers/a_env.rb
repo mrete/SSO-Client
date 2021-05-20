f814096fdc4d2d504c9152523f5ab65f2ad17822
Mihaela Rete <mihaela.rete@takeofflabs.com>
2021-05-20 11:22:23 +0300

    change uid&secret

--- config/initializers/a_env.rb
+++ config/initializers/a_env.rb
@@ -3,5 +3,5 @@ ENV['SEARCH_GOLFER_URL'] ||= "https://api-stage.hcp2020.com/api/v1/golfers.json?
 ENV['SEARCH_GUARDIAN_URL'] ||= "https://api-stage.hcp2020.com/api/v1/guardians"
 ENV['VENDOR_USER_EMAIL'] ||= 'sampleappclient@noemail.com'
 ENV['VENDOR_USER_PASSWORD'] ||= '123456'
-ENV["OAUTH_ID"] ||= 'nJG7Oopi3ddgblixAlUGxmj9H9Ef1A7ZD5PVLimLeck'
-ENV["OAUTH_SECRET"] ||= 'S_aBXmqAkYWbXqNwvhcCSz50k_IjhWDdkFtVoy4M8YE'
+ENV["OAUTH_ID"] ||= 'KM1wc07AXLzo6CvG5DpbiOvTjl4wnK6Z9h8KJesEMQI'
+ENV["OAUTH_SECRET"] ||= 'BtXydwRZd8Hlcw8EjDvFPwH0rIJj9QjOICkWoUNHwuw'
