a42b49cc157124ffe3ea63fdcfaf469652e7af37
Mihaela Rete <mihaela.rete@takeofflabs.com>
2021-06-08 12:29:54 +0300

    changes for development

--- config/initializers/a_env.rb
+++ config/initializers/a_env.rb
@@ -1,7 +1,7 @@
-ENV['LOGIN_URL'] ||= "https://api-stage.hcp2020.com/api/v1/users/login.json"
-ENV['SEARCH_GOLFER_URL'] ||= "https://api-stage.hcp2020.com/api/v1/golfers.json?page=1&per_page=25&global_search=true&sorting_criteria=full_name&order=asc"
-ENV['SEARCH_GUARDIAN_URL'] ||= "https://api-stage.hcp2020.com/api/v1/guardians"
-ENV['VENDOR_USER_EMAIL'] ||= 'sampleappclient@noemail.com'
+ENV['LOGIN_URL'] ||= "http://api2.ghin.com/api/v1/users/login.json"
+ENV['SEARCH_GOLFER_URL'] ||= "http://api2.ghin.com/api/v1/golfers.json?page=1&per_page=25&global_search=true&sorting_criteria=full_name&order=asc"
+ENV['SEARCH_GUARDIAN_URL'] ||= "http://api2.ghin.com/api/v1/guardians"
+ENV['VENDOR_USER_EMAIL'] ||= 'danamarchis.testing9+sampleappclient@gmail.com'
 ENV['VENDOR_USER_PASSWORD'] ||= '123456'
-ENV["OAUTH_ID"] ||= 'KM1wc07AXLzo6CvG5DpbiOvTjl4wnK6Z9h8KJesEMQI'
-ENV["OAUTH_SECRET"] ||= 'BtXydwRZd8Hlcw8EjDvFPwH0rIJj9QjOICkWoUNHwuw'
+ENV["OAUTH_ID"] ||= 'itQZxpZg4__hokIvc90RNyObNW0ljBOuOQBT4HSVGNo'
+ENV["OAUTH_SECRET"] ||= 'mJKrSZ97rGdp8onBoNMNXAx53Gb_frMUdurSrvPXvHs'
--- lib/omniauth/strategies/vendor.rb
+++ lib/omniauth/strategies/vendor.rb
@@ -4,7 +4,7 @@ module OmniAuth
       option :name, :vendor
 
       option :client_options, {
-        site: "https://api-stage.hcp2020.com",
+        site: "http://api2.ghin.com",
         authorize_path: "/oauth/authorize"
       }
 
